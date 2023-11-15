1326a07dac3e9db2e4301115ba5827cf40cd0c21
Bernd Lörwald <bloerwald@googlemail.com>
2023-11-15 17:05:03 +0100

    add ability to filter bounty boards / activity icons

--- MapCleaner.lua
+++ MapCleaner.lua
@@ -13,6 +13,8 @@ local COMMAND_FILTERVIGNETTE = 'filtervignette'
 local COMMAND_UNFILTERVIGNETTE = 'unfiltervignette'
 local COMMAND_FILTERQUEST = 'filterquest'
 local COMMAND_UNFILTERQUEST = 'unfilterquest'
+local COMMAND_FILTERBOUNTYBOARD = 'filterbountyboard'
+local COMMAND_UNFILTERBOUNTYBOARD = 'unfilterbountyboard'
 local COMMAND_LISTFILTERED = 'listfiltered'
 local COMMAND_LISTVISIBLE = 'listvisible'
 
@@ -21,59 +23,75 @@ local L = {
     ['ADDED_POI'] = ADDON_NAME .. ': Added POI %s (%d) to filter list.',
     ['ADDED_VIGNETTE'] = ADDON_NAME .. ': Added vignette %s (%d) to filter list.',
     ['ADDED_QUEST'] = ADDON_NAME .. ': Added quest %s (%d) to filter list.',
+    ['ADDED_BOUNTYBOARD'] = ADDON_NAME .. ': Added map %s (%d) to filter list for bounty boards.',
+    ['ADDED_BOUNTYBOARD_WARN_NO_SUCH'] = ADDON_NAME .. ': Added map %s (%d) to filter list for bounty boards. Note that there currently was no bounty visible there.',
     ['REMOVED_POI'] = ADDON_NAME .. ': Removed POI %s (%d) from filter list.',
     ['REMOVED_VIGNETTE'] = ADDON_NAME .. ': Removed vignette %s (%d) from filter list.',
     ['REMOVED_QUEST'] = ADDON_NAME .. ': Removed quest %s (%d) from filter list.',
+    ['REMOVED_BOUNTYBOARD'] = ADDON_NAME .. ': Removed map %s (%d) from filter list for bounty boards.',
     ['LIST_ITEM_NAME_ID_PAIR'] = '- %s (%d)',
     ['LIST_FILTERED_EMPTY'] = ADDON_NAME .. ': No filters at all.',
     ['LIST_FILTERED_VIGNETTES'] = ADDON_NAME .. ': Filtered vignettes:',
     ['LIST_FILTERED_POIS'] = ADDON_NAME .. ': Filtered area POIs:',
     ['LIST_FILTERED_QUESTS'] = ADDON_NAME .. ': Filtered area quests:',
+    ['LIST_FILTERED_BOUNTYBOARDS'] = ADDON_NAME .. ': Filtered bounty boards:',
     ['LIST_VISIBLE_EMPTY'] = ADDON_NAME .. ': No visible vignettes, POIs or quests at all.',
     ['LIST_VISIBLE_VIGNETTES'] = ADDON_NAME .. ': Visible vignettes:',
     ['LIST_VISIBLE_POIS'] = ADDON_NAME .. ': Visible area POIs:',
     ['LIST_VISIBLE_QUESTS'] = ADDON_NAME .. ': Visible area quests:',
+    ['LIST_VISIBLE_BOUNTYBOARDS'] = ADDON_NAME .. ': Maps with currently visible bounty boards:',
     ['HELP_LINE_FILTERPOI'] = '- add a filter for an area POI: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERPOI .. ' id',
     ['HELP_LINE_FILTERVIGNETTE'] = '- add a filter for a vignette: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERVIGNETTE .. ' id',
     ['HELP_LINE_FILTERQUEST'] = '- add a filter for a quest: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERQUEST .. ' id',
+    ['HELP_LINE_FILTERBOUNTYBOARD'] = '- add a filter for a bounty board: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERBOUNTYBOARD .. ' id. Use id "current" for the currently shown one.',
     ['HELP_LINE_UNFILTERPOI'] = '- remove a filter for an area POI: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERPOI .. ' id',
     ['HELP_LINE_UNFILTERVIGNETTE'] = '- remove a filter for a vignette: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERVIGNETTE .. ' id',
     ['HELP_LINE_UNFILTERQUEST'] = '- remove a filter for a quest: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERQUEST .. ' id',
+    ['HELP_LINE_UNFILTERBOUNTYBOARD'] = '- remove a filter for a bounty boad: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERBOUNTYBOARD .. ' id',
     ['HELP_LINE_LISTVISIBLE'] = '- show all currently visible items: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_LISTVISIBLE .. ' -- it is strongly recommended to use an Addon like idTip instead!',
     ['HELP_LINE_LISTFILTERED'] = '- show all current filters: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_LISTFILTERED,
     ['HELP_LINE_SHORTCUT'] = 'You can use either ' .. SLASH_MAPCLEANER1 .. ' or ' .. SLASH_MAPCLEANER2 .. '.',
     ['ERROR_NO_SUCH_FILTERED_POI_ID'] = ADDON_NAME .. ': No filtered area POI with id %d ("%s").',
     ['ERROR_NO_SUCH_FILTERED_VIGNETTE_ID'] = ADDON_NAME .. ': No filtered vignette with id %d ("%s").',
     ['ERROR_NO_SUCH_FILTERED_QUEST_ID'] = ADDON_NAME .. ': No filtered quest with id %d ("%s").',
+    ['ERROR_NO_SUCH_FILTERED_BOUNTYBOARD_MAPID'] = ADDON_NAME .. ': No filtered bounty board for map %d ("%s").',
   },
   ['deDE'] = {
     ['ADDED_POI'] = ADDON_NAME .. ': POI %s (%d) zur Filterliste hinzugefügt.',
     ['ADDED_VIGNETTE'] = ADDON_NAME .. ': Vignette %s (%d) zur Filterliste hinzugefügt.',
     ['ADDED_QUEST'] = ADDON_NAME .. ': Quest %s (%d) zur Filterliste hinzugefügt.',
+    ['ADDED_BOUNTYBOARD'] = ADDON_NAME .. ': Karte %s (%d) zur Filterliste für Weltquestbonusanzeige hinzugefügt.',
+    ['ADDED_BOUNTYBOARD_WARN_NO_SUCH'] = ADDON_NAME .. ': Karte %s (%d) zur Filterliste für Weltquestbonusanzeige hinzugefügt. Achtung: Dort war gerade gar keine Anzeige.',
     ['REMOVED_POI'] = ADDON_NAME .. ': POI %s (%d) aus Filterliste gelöscht.',
     ['REMOVED_VIGNETTE'] = ADDON_NAME .. ': Vignette %s (%d) aus Filterliste gelöscht.',
     ['REMOVED_QUEST'] = ADDON_NAME .. ': Quest %s (%d) aus Filterliste gelöscht.',
+    ['REMOVED_BOUNTYBOARD'] = ADDON_NAME .. ': Karte %s (%d) aus Filterliste für Weltquestbonusanzeige gelöscht.',
     ['LIST_ITEM_NAME_ID_PAIR'] = '- %s (%d)',
     ['LIST_FILTERED_EMPTY'] = ADDON_NAME .. ': Kein einziger Filter.',
     ['LIST_FILTERED_VIGNETTES'] = ADDON_NAME .. ': Gefilterte Vignetten:',
     ['LIST_FILTERED_POIS'] = ADDON_NAME .. ': Gefilterte Gebiets-POIs:',
     ['LIST_FILTERED_QUESTS'] = ADDON_NAME .. ': Gefilterte Quests:',
+    ['LIST_FILTERED_BOUNTYBOARDS'] = ADDON_NAME .. ': Gefilterte Weltquestbonusanzeigen:',
     ['LIST_VISIBLE_EMPTY'] = ADDON_NAME .. ': Kein einziger sichtbarer Gebiets-POI, Vignette oder Quest.',
     ['LIST_VISIBLE_VIGNETTES'] = ADDON_NAME .. ': Sichtbare Vignetten:',
     ['LIST_VISIBLE_POIS'] = ADDON_NAME .. ': Sichtbare Gebiets-POIs:',
     ['LIST_VISIBLE_QUESTS'] = ADDON_NAME .. ': Sichtbare Quests:',
+    ['LIST_VISIBLE_BOUNTYBOARDS'] = ADDON_NAME .. ': Karten mit zur Zeit sichtbaren Weltquestbonusanzeigen:',
     ['HELP_LINE_FILTERPOI'] = '- Füge einen Filter für einen Gebiets POI hinzu: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERPOI .. ' id',
     ['HELP_LINE_FILTERVIGNETTE'] = '- Füge einen Filter für eine Vignette hinzu: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERVIGNETTE .. ' id',
     ['HELP_LINE_FILTERQUEST'] = '- Füge einen Filter für eine Quest hinzu: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERQUEST .. ' id',
+    ['HELP_LINE_FILTERBOUNTYBOARD'] = '- Füge einen Filter für die Weltquestbonusanzeige einer Weltkarte hinzu: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_FILTERBOUNTYBOARD .. ' id. Nutze die ID "current" für die aktuell offene.',
     ['HELP_LINE_UNFILTERPOI'] = '- Entferne einen Filter für einen Gebiets POI: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERPOI .. ' id',
     ['HELP_LINE_UNFILTERVIGNETTE'] = '- Entferne einen Filter für eine Vignette: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERVIGNETTE .. ' id',
     ['HELP_LINE_UNFILTERQUEST'] = '- Entferne einen Filter für eine Quest: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERQUEST .. ' id',
+    ['HELP_LINE_UNFILTERBOUNTYBOARD'] = '- Entferne einen Filter für eine Weltquestbonusanzeige: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_UNFILTERBOUNTYBOARD .. ' id',
     ['HELP_LINE_LISTVISIBLE'] = '- Zeige alle derzeit sichtbaren Dinge: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_LISTVISIBLE .. ' -- Die alternative Nutzung von einem Addon wie idTip ist sehr stark empfohlen!',
     ['HELP_LINE_LISTFILTERED'] = '- Zeige die aktuell eingestellten Filter: ' .. SLASH_MAPCLEANER1 .. ' ' .. COMMAND_LISTFILTERED,
     ['HELP_LINE_SHORTCUT'] = 'Du kannst entweder ' .. SLASH_MAPCLEANER1 .. ' oder ' .. SLASH_MAPCLEANER2 .. ' nutzen.',
     ['ERROR_NO_SUCH_FILTERED_POI_ID'] = ADDON_NAME .. ': Kein gefilteter Gebiets-POI mit ID %d ("%s").',
     ['ERROR_NO_SUCH_FILTERED_VIGNETTE_ID'] = ADDON_NAME .. ': Keine gefilterte Vignette mit ID %d ("%s").',
     ['ERROR_NO_SUCH_FILTERED_QUEST_ID'] = ADDON_NAME .. ': Keine gefilterte Quest mit ID %d ("%s").',
+    ['ERROR_NO_SUCH_FILTERED_BOUNTYBOARD_MAPID'] = ADDON_NAME .. ': Keine gefilterte Weltquestbonusanzeige für Karte %d ("%s").',
   },
 }
 local currentLocale = GetLocale()
@@ -91,11 +109,10 @@ function MapCleaner:Startup()
   MAPCLEANER_FILTERED_VIGNETTES = MAPCLEANER_FILTERED_VIGNETTES or {}
   MAPCLEANER_FILTERED_POIS = MAPCLEANER_FILTERED_POIS or {}
   MAPCLEANER_FILTERED_QUESTS = MAPCLEANER_FILTERED_QUESTS or {}
+  MAPCLEANER_FILTERS = MAPCLEANER_FILTERS or {}
+  MAPCLEANER_FILTERS.bountyBoards = MAPCLEANER_FILTERS.bountyBoards or {}
 
   local orig_VignetteDataProviderMixin_ShouldShowVignette = nil
-  local orig_AreaPOIDataProviderMixin_RefreshAllData = nil
-  local orig_MapUtil_ShouldShowTask = nil
-
   function VignetteDataProviderMixin_ShouldShowVignette(self, vignetteInfo)
      return orig_VignetteDataProviderMixin_ShouldShowVignette(self, vignetteInfo) and
             MAPCLEANER_FILTERED_VIGNETTES[vignetteInfo.vignetteID] == nil
@@ -120,9 +137,29 @@ function MapCleaner:Startup()
     end
   end
 
+  local orig_MapUtil_ShouldShowTask = nil
   function MapUtil_ShouldShowTask(mapID, info)
     return orig_MapUtil_ShouldShowTask(mapID, info) and
            MAPCLEANER_FILTERED_QUESTS[info.questId] == nil
+  end
+
+  function post_WorldMapBountyBoardMixin_Refresh(self)
+    if MapCleaner:ShouldHideBountyBoardForMap(self:GetMapID()) then
+      self:Clear()
+    end
+  end
+  function post_WorldMapActivityTrackerMixin_Refresh(self)
+    if MapCleaner:ShouldHideBountyBoardForMap(self:GetMapID()) then
+      self:Clear()
+    end
+  end
+  function post_WorldMapThreatFrameMixin_Refresh(self)
+    if MapCleaner:ShouldHideBountyBoardForMap(self:GetParent():GetMapID()) then
+      self.Background:Hide()
+      self.Eye:Hide()
+      self.ModelSceneTop:Hide()
+      self.ModelSceneBottom:Hide()
+    end
   end
 
   for dp,_ in pairs(WorldMapFrame.dataProviders) do
@@ -132,7 +169,6 @@ function MapCleaner:Startup()
         self.refreshVignettes = function() dp:RefreshAllData(false) end -- todo: does not work
      end
      if dp.RemoveAllData and dp.RemoveAllData == AreaPOIDataProviderMixin.RemoveAllData then
-        orig_AreaPOIDataProviderMixin_RefreshAllData = dp.RefreshAllData
         dp.RefreshAllData = AreaPOIDataProviderMixin_RefreshAllData
         self.refreshPOIs = function() dp:RefreshAllData(false) end
      end
@@ -142,6 +178,26 @@ function MapCleaner:Startup()
         self.refreshQuests = function() dp:RefreshAllData(false) end
      end
   end
+
+  local refreshBountyBoard = function() end
+  for _,of in pairs(WorldMapFrame.overlayFrames) do
+    if of.Refresh and of.Refresh == WorldMapBountyBoardMixin.Refresh then
+      hooksecurefunc(of, "Refresh", post_WorldMapBountyBoardMixin_Refresh)
+      local lastRefreshBountyBoard = refreshBountyBoard
+      refreshBountyBoard = function() lastRefreshBountyBoard(); of:Refresh() end
+    end
+    if of.Refresh and of.Refresh == WorldMapThreatFrameMixin.Refresh then
+      hooksecurefunc(of, "Refresh", post_WorldMapThreatFrameMixin_Refresh)
+      local lastRefreshBountyBoard = refreshBountyBoard
+      refreshBountyBoard = function() lastRefreshBountyBoard(); of:Refresh() end
+    end
+    if of.Refresh and of.Refresh == WorldMapActivityTrackerMixin.Refresh then
+      hooksecurefunc(of, "Refresh", post_WorldMapActivityTrackerMixin_Refresh)
+      local lastRefreshBountyBoard = refreshBountyBoard
+      refreshBountyBoard = function() lastRefreshBountyBoard(); of:Refresh() end
+    end
+  end
+  self.refreshBountyBoard = refreshBountyBoard
 end
 
 local PROBABLY_MORE_THAN_MAP_ID = 4000
@@ -282,7 +338,6 @@ function MapCleaner:AllVisibleQuests()
   return quests
 end
 
-
 function MapCleaner:AddFilterForQuest(id)
   local name = self:TryGetQuestName(id)
 
@@ -308,11 +363,75 @@ function MapCleaner:RemoveFilterForQuest(id)
   print(format(L.REMOVED_QUEST, name, id))
 end
 
+
+function MapCleaner:GetMapName(mapId)
+  self.cachedMapNames = self.cachedMapNames or {}
+  if self.cachedMapNames[mapId] == nil then
+    local name = C_Map.GetMapInfo(mapId).name
+    if name ~= "" then
+      self.cachedMapNames[mapId] = name
+    end
+  end
+  return self.cachedMapNames[mapId] or SENTINEL_UNKNOWN_NAME
+end
+
+function MapCleaner:AllVisibleBountyBoards()
+  local mapsWithBountyBoard = {}
+  for mapId = 1, PROBABLY_MORE_THAN_MAP_ID do
+    if MapUtil.MapHasEmissaries(mapId) then
+      mapsWithBountyBoard[mapId] = self:GetMapName(mapId)
+    end
+  end
+  return mapsWithBountyBoard
+end
+
+function MapCleaner:AddFilterForBountyBoard(mapId)
+  local mapName = self:GetMapName(mapId)
+
+  MAPCLEANER_FILTERS.bountyBoards[mapId] = mapName
+
+  self:refreshBountyBoard()
+
+  if MapUtil.MapHasEmissaries(mapId) then
+    print(format(L.ADDED_BOUNTYBOARD, mapName, mapId))
+  else
+    print(format(L.ADDED_BOUNTYBOARD_WARN_NO_SUCH, mapName, mapId))
+  end
+end
+
+function MapCleaner:RemoveFilterForBountyBoard(mapId)
+  if MAPCLEANER_FILTERS.bountyBoards[mapId] == nil then
+    print(format(L.ERROR_NO_SUCH_FILTERED_BOUNTYBOARD_MAPID, mapId, self:GetMapName(id)))
+    return
+  end
+
+  local mapName = MAPCLEANER_FILTERS.bountyBoards[mapId]
+
+  MAPCLEANER_FILTERS.bountyBoards[mapId] = nil
+
+  self:refreshBountyBoard()
+
+  print(format(L.REMOVED_BOUNTYBOARD, mapName, mapId))
+end
+
+function MapCleaner:ShouldHideBountyBoardForMap(mapId)
+  while mapId ~= nil and mapId ~= 0 do
+    if MAPCLEANER_FILTERS.bountyBoards[mapId] then
+      return true
+    end
+
+    mapId = C_Map.GetMapInfo(mapId).parentMapID
+  end
+
+  return false
+end
+
 function MapCleaner:ListFiltered()
   local hasVignettes = next(MAPCLEANER_FILTERED_VIGNETTES) ~= nil
   local hasPOIs = next(MAPCLEANER_FILTERED_POIS) ~= nil
   local hasQuests = next(MAPCLEANER_FILTERED_QUESTS) ~= nil
-  if not hasVignettes and not hasPOIs and not hasQuests then
+  local hasBountyBoards = next(MAPCLEANER_FILTERS.bountyBoards) ~= nil
+  if not hasVignettes and not hasPOIs and not hasQuests and not hasBountyBoards then
     print(format(L.LIST_FILTERED_EMPTY))
   end
   if hasVignettes then
@@ -345,16 +464,28 @@ function MapCleaner:ListFiltered()
       print(format(L.LIST_ITEM_NAME_ID_PAIR, name, id))
     end
   end
+  if hasBountyBoards then
+    print(format(L.LIST_FILTERED_BOUNTYBOARDS))
+    for mapId, mapName in pairs(MAPCLEANER_FILTERS.bountyBoards) do
+      if mapName == SENTINEL_UNKNOWN_NAME then
+        mapName = self:GetMapName(mapId)
+        MAPCLEANER_FILTERS.bountyBoards[mapId] = mapName
+      end
+      print(format(L.LIST_ITEM_NAME_ID_PAIR, mapName, mapId))
+    end
+  end
 end
 
 function MapCleaner:ListVisible()
   local allVignettes = self:AllVisibleVignettes()
   local allPOIs = self:AllVisiblePOIs()
   local allQuests = self:AllVisibleQuests()
+  local allBountyBoards = self:AllVisibleBountyBoards()
   local hasVignettes = next(allVignettes) ~= nil
   local hasPOIs = next(allPOIs) ~= nil
   local hasQuests = next(allQuests) ~= nil
-  if not hasVignettes and not hasPOIs and not hasQuests then
+  local hasBountyBoards = next(allQuests) ~= nil
+  if not hasVignettes and not hasPOIs and not hasQuests and not hasBountyBoards then
     print(format(L.LIST_VISIBLE_EMPTY))
   end
   if hasVignettes then
@@ -372,6 +503,12 @@ function MapCleaner:ListVisible()
   if hasQuests then
     print(format(L.LIST_VISIBLE_QUESTS))
     for id, name in pairs(allQuests) do
+      print(format(L.LIST_ITEM_NAME_ID_PAIR, name, id))
+    end
+  end
+  if hasBountyBoards then
+    print(format(L.LIST_VISIBLE_BOUNTYBOARDS))
+    for id, name in pairs(allBountyBoards) do
       print(format(L.LIST_ITEM_NAME_ID_PAIR, name, id))
     end
   end
@@ -397,6 +534,12 @@ function MapCleaner:Cli(line)
   elseif op == COMMAND_UNFILTERQUEST then
     local id = tonumber(rest)
     MapCleaner:RemoveFilterForQuest(id)
+  elseif op == COMMAND_FILTERBOUNTYBOARD then
+    local id = rest == 'current' and WorldMapFrame:GetMapID() or tonumber(rest)
+    MapCleaner:AddFilterForBountyBoard(id)
+  elseif op == COMMAND_UNFILTERBOUNTYBOARD then
+    local id = rest == 'current' and WorldMapFrame:GetMapID() or tonumber(rest)
+    MapCleaner:RemoveFilterForBountyBoard(id)
   elseif op == COMMAND_LISTFILTERED then
     MapCleaner:ListFiltered()
   elseif op == COMMAND_LISTVISIBLE then
@@ -406,9 +549,11 @@ function MapCleaner:Cli(line)
     print(format(L.HELP_LINE_FILTERPOI))
     print(format(L.HELP_LINE_FILTERVIGNETTE))
     print(format(L.HELP_LINE_FILTERQUEST))
+    print(format(L.HELP_LINE_FILTERBOUNTYBOARD))
     print(format(L.HELP_LINE_UNFILTERPOI))
     print(format(L.HELP_LINE_UNFILTERVIGNETTE))
     print(format(L.HELP_LINE_UNFILTERQUEST))
+    print(format(L.HELP_LINE_UNFILTERBOUNTYBOARD))
     print(format(L.HELP_LINE_LISTVISIBLE))
     print(format(L.HELP_LINE_LISTFILTERED))
     print(format(L.HELP_LINE_SHORTCUT))
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # MapCleaner
 
-Filter annoying WoW world map clutter! You define what is shown there, not Blizzard. Just add area POI, vignette or quest IDs and to a filter list and enjoy a plain map. Do you want spam
+Filter annoying WoW world map clutter! You define what is shown there, not Blizzard. Just add area POI, vignette or quest IDs or remove the bounty advertisment via a map ID and to a filter list and enjoy a plain map. Do you want spam
 
-![A screenshot of Zaralekk Cavern on a dead realm with loads of rares up.](./.readme/before.png)
+![A screenshot of Zaralekk Cavern on an active realm with loads of rares up.](./.readme/before.png)
 
 or just what you're interested in and nothing more?
 
@@ -16,6 +16,7 @@ Use the `/mapcleaner` or `/mc` slash command:
 - `/mc filtervignette 1331` removes vignette 1331 from your map.
 - `/mc filterpoi 1331` removes area POI 1331 from your map.
 - `/mc filterquest 1331` removes quest 1331 from your map.
-- `/mc unfiltervignette 1331`, `/mc unfilterpoi 1331` and `/mc unfilterquest 1331` reverses that.
+- `/mc filterbountyboard 1331` removes the bounty board or activity reminder from map 1331. `/mc filterbountyboard current` does the same for the currently opened map.
+- `/mc unfiltervignette 1331`, `/mc unfilterpoi 1331`, `/mc unfilterquest 1331` and `/mc unfilterbountyboard 1331` (or `… current`) reverses that.
 - `/mc listfiltered` gives you a list with everything you removed.
 - `/mc listvisible` gives a load of POIs, vignettes and quests that can be filtered. The [idTip](https://github.com/ItsJustMeChris/idTip-Community-Fork) addon is **way** superior to this list since it allows you to hover an annoying icon and just see the ID rather than searching in this way too long list.
